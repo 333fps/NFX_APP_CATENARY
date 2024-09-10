b6530bc7864a053d93a434f7a8473aad2c65063b
333fps <ronan.fesselier@gmail.com>
2024-09-10 07:35:28 +0200

    Added Copy Ctor

--- main.cpp
+++ main.cpp
@@ -9,11 +9,17 @@ int main(int argc, char* argv[])
 	(void)argc;
 	(void)argv;
 
+	Catenary c;
+
 	Catenary::Height h{ 1500. };
 	Catenary::Laydown ld{ 500. };
 	Catenary::LinearDensity l{ 2.0 };
 
-	Catenary c{ h, ld, l };
+	c.update(h, ld, l);
+
+	auto copy = Catenary{ c };
+
+	c.reset();
 
 	std::cout << " Linear density....: " << c.linearDensity() << std::endl;
 	std::cout << " Top tension.......: " << c.topTension() << std::endl;
@@ -24,4 +30,15 @@ int main(int argc, char* argv[])
 	std::cout << " Suspended length..: " << c.suspendedLength() << std::endl;
 	std::cout << " Laydown...........: " << c.laydown() << std::endl;
 	std::cout << " Sag parameter.....: " << c.sagParameter() << std::endl;
+
+	std::cout << "\nCopy" << std::endl;
+	std::cout << " Linear density....: " << copy.linearDensity() << std::endl;
+	std::cout << " Top tension.......: " << copy.topTension() << std::endl;
+	std::cout << " Alpha angle.......: " << copy.alphaAngle() << std::endl;
+	std::cout << " Height............: " << copy.height() << std::endl;
+	std::cout << " Layback...........: " << copy.layback() << std::endl;
+	std::cout << " Horizontal tension: " << copy.horizontalTension() << std::endl;
+	std::cout << " Suspended length..: " << copy.suspendedLength() << std::endl;
+	std::cout << " Laydown...........: " << copy.laydown() << std::endl;
+	std::cout << " Sag parameter.....: " << copy.sagParameter() << std::endl;
 }
